35cc3494d6c9668c28b2172adf69e6330a91d04e
sergioq7777 <sergioq.7777@gmail.com>
2019-10-28 22:26:26 +0000

    test

--- _printf.c
+++ _printf.c
@@ -9,7 +9,65 @@
 
 int _printf(const char *format, ...)
 {
+	p_let array[] = {
 
+		{"c", _character},
+		{"s", _string},
+		{"\0", NULL},
+	};
 
+	int x, y, z;
+	va_list list;
+
+	if (!format)
+	{
+		return (-1);
+	}
+	va_start(list, format);
+
+	x = 0;
+	z = 0;
+
+	if (x == 0 && format[x + 1] == '\0')
+		return (-1);
+
+	while (format[x] != '\0')
+	{
+		if (format[x] != '%')
+		{
+			y = 0;
+
+			while (array[y].f != '\0')
+			{
+				if (array[y].p == format[x + 1])
+				{
+					z += array[y].ptr(list);
+					x++;
+					break;
+				}
+				y++;
+			}
+
+			if (formar[x] == '%' && format[x + 1] == '%')
+			{
+				_putchar('%');
+				x++;
+				z++;
+				break;
+			}
+			if (formar[x] == '%' && format[x + 1] == '\0')
+			{
+				_putchar('%');
+			}
+		}
+		else
+		{
+			_putchar(format[c1]);
+			z++;
+		}
+		x++;
+	}
+	va_end(list);
+	return (z);
 
 }
