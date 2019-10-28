ff3d7eada395e07d24e8c68626740786f33a1447
sergioq7777 <sergioq.7777@gmail.com>
2019-10-28 22:25:38 +0000

    .h with the first lib

--- holberton.h
+++ holberton.h
@@ -5,8 +5,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <unistd.h>
 
+
+int _putchar(char c);
 int _printf(const char *format, ...);
+int _character(va_list list);
+int _string(va_list list);
+int _strlen_recursion(char *s);
 
+typedef struct argumentos
+{
+	char p;
+	int (*ptr)(va_list);
+} p_let;
 
 #endif
