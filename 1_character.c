ef79c7197801a77a40005582a314bf085d057a1f
sergioq7777 <sergioq.7777@gmail.com>
2019-10-29 20:49:35 +0000

    fix char

--- 1_character.c
+++ 1_character.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
-
+ *
+ *
+ *
  */
 
-int _character(va_list list)
+int f_ch(va_list list)
 {
 	char i;
 
 	i = va_arg(list, int);
-
 	if (i == 0)
 		return (0);
 	_putchar(i);
-
 	return (1);
 }
