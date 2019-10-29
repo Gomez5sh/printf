1b8a300554b8835dbfcc06f5ded329a596e34a67
sergioq7777 <sergioq.7777@gmail.com>
2019-10-29 14:33:10 +0000

    strlen function

--- 3_strlen.c
+++ 3_strlen.c
@@ -7,9 +7,11 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	int l = 0;
+
+	while (s[l] != '\0')
 	{
-		return ((_strlen_recursion(s + 1)) + 1);
+		l++;
 	}
-	return (0);
+	return (l);
 }
