1af5a2e0c7929316b8ab8d89488245d4b49b3117
sergioq7777 <sergioq.7777@gmail.com>
2019-10-29 23:15:08 +0000

    [ADD] some things

--- 2_string.c
+++ 2_string.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
+/**
+ * f_str - check the code for Holberton School students.
+ *@list: va_list
+ *
+ * Return: Always.
+ */
 
 int f_str(va_list list)
 {
 	char *string;
 	unsigned int y, l;
 
-	string = va_arg(list ,char *);
+	string = va_arg(list, char *);
 	if (string == NULL)
 		string = "(null)";
 
