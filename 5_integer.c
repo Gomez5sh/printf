b378d13a088cc0c0e57366869828578e9c0dfb31
sergioq7777 <sergioq.7777@gmail.com>
2019-10-31 01:35:34 +0000

    add correction

--- 5_integer.c
+++ 5_integer.c
@@ -47,3 +47,81 @@ int f_int(va_list list)
 
 	return (i);
 }
+
+/**
+ * f_int - print integers
+ *@list: argument entered to print
+ *
+ * Return: always
+ */
+int f_oct(va_list list)
+{
+	unsigned int len;
+
+	len = convert_num(list, 8);
+	return (len);
+}
+
+
+/**
+ * f_int - print integers
+ *@list: argument entered to print
+ *
+ * Return: always
+ */
+int f_hex(va_list list)
+{
+	unsigned int len;
+
+	len = convert_num(list, 16);
+	return (len);
+}
+
+/**
+ * f_int - print integers
+ *@list: argument entered to print
+ *
+ * Return: always
+ */
+int f_base(unsigned int number, int base)
+{
+
+	int i = 0;
+
+	while (number / base != 0)
+	{
+		number /= base;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * f_int - print integers
+ *@list: argument entered to print
+ *
+ * Return: always
+ */
+int convert_num(va_list list, int base)
+{
+
+	char low[] = "0123456789abcdef";
+	char *mynum;
+	unsigned int numc = va_arg(list, unsigned int);
+	int len = f_base(numc, base);
+	int i = 0;
+
+	mynum = malloc(len +1);
+
+	while (numc/base != 0)
+	{
+		mynum[--len] = low[numc % base];
+		numc = numc / base;
+	}
+	while (mynum[i] != '\0')
+	{
+		_putchar(mynum[i++]);
+	}
+	return(i);
+
+}
